786aef604e2af9b4a4b19540db949341fbd4fcda
Shofi <kudefajri@gmail.com>
2022-04-25 11:46:22 +0700

    menambakan input pda main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+# menerima input dari user:
+# angka 1
+angka1 = int(input("Masukkan angka pertama: "))
+
+#angka 2
+angka2 = int(input("Masukkan angka kedua: "))
+
+# operasi yang dilakukan
+	# 1 penjumlahan
+	# 2 pengurangan	
+	# 3 pembagian
+	# 4 perkalian
+
+print("""Masukkan operasi:
+	1 : penjumlahan
+	2 : pengurangan
+	3 : pembagian
+	4 : perkalian""")
+
+operasi = int(input("Masukkan operasi"))
+
+
